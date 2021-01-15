2ac15b423e58904acb7087b03bd4cc9a57bdeee4
David Yeung <quxbaz@gmail.com>
2021-01-14 21:08:59 -0700

    Notes.

--- src/recharts-test/Notes.js
+++ src/recharts-test/Notes.js
@@ -8,17 +8,24 @@ const Notes = ({className}) => (
     <ul>
       <li>Dynamic resizing is supported.</li>
       <li>
-        recharts is quite a large package and draws in a large number of dependencies.<br /><br />
+        recharts is quite a large package and draws in a large number of dependencies.
+      </li>
+      <br />
+      <li>
         <a href="./bundle-size.png" style={{float: 'right', marginLeft: 10}}>
           <img src="./bundle-size.png" width={120} height={120} />
         </a>
-        Total bundle size with <b>recharts</b>, <b>react</b>, and <b>react-dom</b> is <b>512K</b> uncompressed and <b>148K</b> compressed.<br />
-        <div style={{clear: 'both'}} /><br />
+        Total bundle size with <b>recharts</b>, <b>react</b>, and <b>react-dom</b> is <b>512K</b> uncompressed and <b>148K</b> compressed.
+      </li>
+      <div style={{clear: 'both'}} />
+      <li>
         <a href="./bundle-size-bare.png" style={{float: 'right', marginLeft: 10}}>
           <img src="./bundle-size-bare.png" width={120} height={120} />
         </a>
-        Total bundle size with <b>react</b> and <b>react-dom</b> is <b>132K</b> uncompressed and <b>44K</b> compressed.<br />
+        Total bundle size with <b>react</b> and <b>react-dom</b> is <b>132K</b> uncompressed and <b>44K</b> compressed.
       </li>
+      <div style={{clear: 'both'}} />
+      <br />
       <li>
         The API is very nice to use. Reactivity is such a nice feature to have
         in a charting library. Declarative programming style fits really well.
